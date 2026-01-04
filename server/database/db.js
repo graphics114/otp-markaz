c2a792408ae67ed708689c5b92eed3be77249782
MUHAMMED UNAIS <unaism010@gmail.com>
2026-01-04 21:23:18 +0530

    Use Render PostgreSQL database

--- server/database/db.js
+++ server/database/db.js
@@ -1,22 +1,19 @@
 import pkg from "pg";
-
 const { Client } = pkg;
 
 const database = new Client({
-    user: "postgres",
-    host: "localhost",
-    database: "ottapalam_markaz",
-    password: "Unaisku114@",
-    port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 try {
-    await database.connect();
-    console.log("Database connected successfully");
+  await database.connect();
+  console.log("Database connected successfully");
 } catch (error) {
-    console.error("Database connected failed:", error);
-    
-    process.exit(1);
+  console.error("Database connection failed:", error);
+  process.exit(1);
 }
 
 export default database;
